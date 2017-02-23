91712684d8afbfde3eafc1cb8650381a0069c3c4
Arjen Mercado <arjen@Arjens-MBP.fios-router.home>
2017-02-23 14:06:58 -0800

    commited CSUF files

---
+++ models/Events.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+var EventSchema = new mongoose.Schema({
+  title: String,
+  time: String,
+  location: String,
+  contact: String,
+  description: String,
+  creator: String,
+  subscribers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Subscriber'}]
+});
+
+mongoose.model('Event', EventSchema);
